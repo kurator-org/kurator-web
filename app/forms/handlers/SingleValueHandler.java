2687217a8eb5c528513f5424e3c9625c18605d23
David Lowery <lowery@cs.umb.edu>
2016-06-15 19:48:41 -0400

    Minor fix to single value handler for support of string array input

--- app/forms/handlers/SingleValueHandler.java
+++ app/forms/handlers/SingleValueHandler.java
@@ -10,8 +10,10 @@ public class SingleValueHandler implements FieldHandler<String> {
     public String transform(Object obj) {
         if (obj instanceof List) {
             return ((List<String>) obj).get(0).toString(); // For single value list
-        } else {
+        } else if (obj instanceof String[]){
             return ((String[])obj)[0];
+        } else {
+            return obj.toString();
         }
     }
 }
