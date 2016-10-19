4d4de5f577bbf884f3784a831213d5f0081bddd7
lowery <lowery@cs.umb.edu>
2016-10-19 16:31:11 -0400

    Adding minimal web application installer code to startup
    Creates the workspace and packages directories if they don't exist and will install jython locally if it can't be found.

--- app/Global.java
+++ app/Global.java
@@ -1,6 +1,10 @@
+import com.typesafe.config.ConfigFactory;
 import models.WorkflowRun;
+import org.python.util.install.Installation;
 import play.*;
 
+import java.io.*;
+import java.security.Permission;
 import java.util.Date;
 import java.util.List;
 
@@ -24,6 +28,56 @@ public class Global extends GlobalSettings {
   @Override
   public void onStop(Application app) {
     Logger.info("Application shutdown...");
-  }  
-    
+  }
+
+  @Override
+  public void beforeStart(Application app) {
+    File packagesDir = new File("packages");
+
+    if (!packagesDir.exists()) {
+      System.out.println("Creating packages directory: " + packagesDir.getAbsolutePath());
+      packagesDir.mkdir();
+    }
+
+    File workspaceDir = new File("workspace");
+
+    if (!workspaceDir.exists()) {
+      System.out.println("Creating workspace directory: " + workspaceDir.getAbsolutePath());
+
+      workspaceDir.mkdir();
+    }
+
+    File jythonDir = new File("jython");
+    if (!jythonDir.exists()) {
+      System.out.println("Jython not found, running installer now...");
+      jythonDir.mkdir();
+
+      //Before running the external Command
+      MySecurityManager secManager = new MySecurityManager();
+      System.setSecurityManager(secManager);
+
+      String[] args = {"-s", "-d", "jython"};
+      Installation.driverMain(args, null, null);
+    }
+  }
+
+  class MySecurityManager extends SecurityManager {
+    @Override public void checkExit(int status) {
+      System.out.println("Installation of kurator-web is complete. Rerun the startup script now to start the server.");
+      File pid = new File("RUNNING_PID");
+      if (pid.exists()) {
+        pid.deleteOnExit();
+      }
+    }
+
+    @Override
+    public void checkPermission(Permission perm) {
+
+    }
+
+    @Override
+    public void checkPermission(Permission perm, Object context) {
+
+    }
+  }
 }
