2142f8178511b7188a44875a8b024aba4a1cddfa
lowery <lowery@fas.harvard.edu>
2018-06-04 13:40:11 -0400

    Startup script for web app that uses authbind to run on port 80

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,6 +1,6 @@
 # --- !Ups
 
-insert into user (id, username, firstname, lastname, email, password, active) values(1, 'admin', 'Administrator', '', 'lowery@fas.harvard.edu', '$2a$10$C6PJ9ED16fpZHSPpapJOxOk7AbjLl9w/wWAC6c4mRURUz4pLCbWTi', TRUE);
+insert into user (id, username, firstname, lastname, email, password, active) values(1, 'admin', 'Administrator', '', 'kurator@localhost', '$2a$10$C6PJ9ED16fpZHSPpapJOxOk7AbjLl9w/wWAC6c4mRURUz4pLCbWTi', TRUE);
 
 insert into security_role (id, name) values(1, 'ADMIN');
 insert into security_role (id, name) values(2, 'USER');
